1b300f6e679058f3ffe8c85e2f45db2d66535692
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-01 14:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  name: name,
+  breed: breed,
+  age: age
+}
+
+function Cat(name, breed, age) {
+  name: name,
+  breed: breed,
+  age: age
+}
+
+function Ferret(name, age, foodRequirements) {
+  name: name,
+  age: age,
+  foodRequirements: foodRequirements
+}
+
+function Bird(name, breed) {
+  name: name,
+  breed: breed
+}
+
+function Fish(name, species, waterConditions) {
+  name: name,
+  species: species,
+  waterConditions: waterConditions
+}
